a20b6b436e684fb636cbf8076d1ef9a839baecb6
Michal <michalgrzegorczyk04@gmail.com>
2021-07-03 11:16:14 +0200

    Add form

--- scripts/script.js
+++ scripts/script.js
@@ -8,4 +8,22 @@ $(function() {
     $('#tabs').tabs();
 });
 
-// 
+// FORM RANGE 
+$(function() {
+    $('#date').datepicker();
+  
+    let $amount = $('#range');
+    let $range = $('#range-slider');
+    $('#range-slider').slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [100, 200],
+        slide: function(event, ui) {
+            $amount.val('$' + ui.values[0] + ' - $' + ui.values[1]);
+        }
+    });
+    $amount
+        .val('$' + $range.slider('values', 0)
+        + ' - $' + $range.slider('values', 1));
+});
