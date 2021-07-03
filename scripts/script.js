98f1c7788f4601238b5162ef4e303933fc1365ff
Michal <michalgrzegorczyk04@gmail.com>
2021-07-03 11:31:17 +0200

    Add comments

--- scripts/script.js
+++ scripts/script.js
@@ -14,16 +14,17 @@ $(function() {
   
     let $amount = $('#range');
     let $range = $('#range-slider');
+    // Event range slider value
     $('#range-slider').slider({
         range: true,
-        min: 0,
-        max: 500,
-        values: [100, 200],
+        min: 75, // min value
+        max: 475, // max value
+        step: 1, // step change default 1
+        values: [112, 185], // start value
         slide: function(event, ui) {
-            $amount.val('$' + ui.values[0] + ' - $' + ui.values[1]);
+            $amount.val(`${ui.values[0]}zł-${ui.values[1]}zł`);
         }
     });
-    $amount
-        .val('$' + $range.slider('values', 0)
-        + ' - $' + $range.slider('values', 1));
+    // Start input value
+    $amount.val(`${$range.slider('values', 0)}zł - ${$range.slider('values', 1)}zł`); 
 });
